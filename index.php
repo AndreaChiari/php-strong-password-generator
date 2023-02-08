bab1c1feb1d0cbe9687a79a229554f12c80256a0
AndreaChiari <chiariandrea94@gmail.com>
2023-02-08 17:21:40 +0100

    update-bugs

--- index.php
+++ index.php
@@ -3,13 +3,25 @@ $pswlength = $_GET['pswlength'] ?? '';
 
 // creo una funzione che mi vada a generare una password randomica in base al valore di input che l'utente inserisce nel form
 
-include './data/includes.php';
-$userpsw = password_generate($pswlength)
+
+function password_generate($chars) 
+{
+    $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz#@$%!?';
+    return substr(str_shuffle($data), 0, $chars);
+}
+
+$userpsw = password_generate($pswlength);
+
+
+//redireziono l'utente in un'altra pagina per mostrargli la password
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,6 +29,7 @@ $userpsw = password_generate($pswlength)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container d-flex justify-content-center">
         <form class="row g-3 mt-5" method="GET">
@@ -26,12 +39,15 @@ $userpsw = password_generate($pswlength)
             </div>
             <div class="col-auto">
                 <label for="inputPassword2" class="visually-hidden"></label>
-                <input type="password" class="form-control" id="inputPassword2" placeholder="lunghezza pasword" name="pswlength" value="<?= $userpsw ?>">
+                <input type="input" class="form-control" id="inputPassword2" placeholder="lunghezza pasword" name="pswlength">
+
             </div>
+            <p><?= $userpsw ?></p>
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary mb-3">Genera password</button>
             </div>
         </form>
     </div>
 </body>
+
 </html>
